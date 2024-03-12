ca2190ea6e62a5f9f47b6d48396814f9d248c42d
Itoodua12 <itoodua@gmail.com>
2024-03-12 22:55:08 +0400

    feature/product rest api

--- main.go
+++ main.go
@@ -19,6 +19,13 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/users/:id", routes.GetUser)
 	app.Put("/api/users/:id", routes.UpdateUser)
 	app.Delete("/api/user/:id", routes.DeleteUser)
+	// Products Endpoints
+	app.Post("/api/products", routes.CreateProduct)
+	app.Get("/api/products", routes.GetProducts)
+	app.Get("/api/products/:id", routes.GetProduct)
+	app.Put("/api/products/:id", routes.UpdateProduct)
+	app.Delete("/api/products/:id", routes.DeleteProduct)
+
 }
 
 func main() {
